176d4d3b7bb438d4998d73b06f60fe1d31017a0f
Your Name <you@example.com>
2022-05-09 20:12:52 -0400

    Differentie entre un emprunteur et un admin

--- bernardtp4/src/component/Connexion.js
+++ bernardtp4/src/component/Connexion.js
@@ -4,9 +4,32 @@ import Button from "./Button";
 
 
 const Connexion = ({utilisateur}) => {
+    const typeUtilisateur = utilisateur.typeUtilisateur;
+    const onClickAjoutUtilisateur = () => {
+    }
+
+    const onClickAjoutDocument = () => {
+    }
+
     return (
         <div>
             <h3>Bienvenue {utilisateur.prenom}, {utilisateur.nom}</h3>
+            {typeUtilisateur === "admin" &&
+                <Button color="blue" text="Ajout Utilisateur" onClick={onClickAjoutUtilisateur}/>
+            }
+            {
+                typeUtilisateur === "admin" &&
+                <Button color="blue" text="Ajout document" onClick={onClickAjoutDocument} />
+            }
+            {   typeUtilisateur === "emprunteur" &&
+                <Button color="blue" text="Faire emprunt" onClick={onClickAjoutUtilisateur}/>
+            }
+            {
+                typeUtilisateur === "emprunteur" &&
+                <Button color="blue" text="Retourner emprunt" onClick={onClickAjoutDocument} />
+            }
+
+
         </div>
     );
 }
